8748d9161d4522c20df07c3ab86b3a24280069a5
Kirill Kleymenov <k.kelolov@gmail.com>
2026-02-24 22:27:12 +0300

    Initialize chezmoi-managed dotfiles

---
+++ dot_zshrc.d/10-shell-setup.zsh
@@ -0,0 +1,9 @@
+eval "$(oh-my-posh init zsh --config '~/.config/oh-my-posh-theme.json')"
+
+source /opt/homebrew/opt/antidote/share/antidote/antidote.zsh
+antidote load ~/.zsh_plugins.txt
+
+bindkey '^[[A' history-substring-search-up
+bindkey '^[[B' history-substring-search-down
+bindkey '^P' history-substring-search-up
+bindkey '^N' history-substring-search-down
